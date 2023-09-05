b4cbaee467171545af74ab8ac3edd9a72f87f0c4
Вадим Довгопол <dovgopol.vad@mail.ru>
2023-09-05 22:26:08 +0300

    none

--- homework.py
+++ homework.py
@@ -14,12 +14,12 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
-
+        message: str = print(f'Тип тренировки: {self.trainig_type}; '
+                             f'Длительность: {self.duration:.3f} ч.; '
+                             f'Дистанция: {self.distance:.3f} км; '
+                             f'Ср. скорость: {self.speed:.3f} км/ч; '
+                             f'Потрачено ккал: {self.calories:.3f}.')
+        return message
 
 class Training:
     """Базовый класс тренировки."""
@@ -51,12 +51,10 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        info = (self.training_type,
-                self.duration,
-                self.get_distance(),
-                self.get_mean_speed(),
-                self.get_spent_calories())
-        return InfoMessage(*info)
+        info = InfoMessage(self.__class__.__name__, self.duration,
+                           self.get_distance(), self.get_mean_speed(),
+                           self.get_spent_calories())
+        return info.get_message()
 
 
 class Running(Training):
@@ -144,8 +142,7 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info = Training.show_training_info(training)
-    print(info.get_message())
+    return Training.show_training_info(training)
 
 
 if __name__ == '__main__':
